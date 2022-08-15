74d575f4718d51f3d8da00a34c69083c60aefe73
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-15 14:33:07 -0700

    Betty changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,28 @@
 #include "sort.h"
 #include <stddef.h>
 
+/**
+ * bubble_sort - sorts the array in numerical order
+ *
+ * @arr: The array to be sorted
+ * @size: The size of arr
+ */
+
 void bubble_sort(int arr[], size_t size)
 {
 	size_t i = 0, j = 0, temp;
 	bool swapped;
 
-	for (i = 0; i < size-1; i++) 
+	for (i = 0; i < size - ; i++)
 	{
 		swapped = false;
-		for (j = 0; j < size-i-1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (arr[j] > arr[j+1])
+			if (arr[j] > arr[j + 1])
 			{
 				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 				swapped = true;
 				print_array(arr, size);
 			}
