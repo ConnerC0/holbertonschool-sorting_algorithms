a466a4abe02296c6f70eece30fcff37c79af3780
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-16 10:31:37 -0700

    added NULL check

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,6 +10,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp1, *temp2, *nodeCheckerF, *nodeCheckerB;
 
+	if (*list == NULL)
+		return;
 	nodeCheckerF = *list;
 	while (nodeCheckerF->next)
 	{
@@ -32,10 +34,8 @@ void insertion_sort_list(listint_t **list)
 					temp2 = nodeCheckerB->next;
 					if (temp1->prev)
 						temp1->prev->next = temp2;
-					temp1->next = temp2->next;
-					temp2->prev = temp1->prev;
-					temp1->prev = temp2;
-					temp2->next = temp1;
+					temp1->next = temp2->next, temp2->prev = temp1->prev;
+					temp1->prev = temp2, temp2->next = temp1;
 					temp1->next->prev = temp1;
 					if (temp2->prev == NULL)
 						*list = temp2;
