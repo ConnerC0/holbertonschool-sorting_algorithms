9cbbf90a2ef4021defe025f2fc3dee9a1b84db61
ConnerC0 <connercurtis72@gmail.com>
2023-01-18 15:09:57 -0600

    correction

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -3,27 +3,6 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-/**
  * swap - Function that swaps two values
  *
  * @a: Fisrt value
