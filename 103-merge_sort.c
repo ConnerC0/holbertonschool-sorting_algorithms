4fc4fe318cb040f4e2cde191aa884394e0ad2e36
ConnerC0 <connercurtis72@gmail.com>
2023-01-18 15:02:59 -0600

    complete

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,27 @@
 #include "sort.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
 
 /**
  * merge_sort - sorts the array with the merge algorithm
@@ -7,9 +29,55 @@
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
-
-
 void merge_sort(int *array, size_t size)
 {
+	int *temp;
 
+	if (!array || size < 2)
+		return;
+
+	temp = malloc(sizeof(*temp) * size);
+	if (!temp)
+		return;
+
+	merge(array, temp, size);
+	free(temp);
+}
+
+/**
+ * _merge_sort - initiate merge sort
+ * @array: array to be sorted
+ * @temp: temporary array for holding sorted elements
+ * @size: size of the array
+ */
+void merge(int *array, int *temp, size_t size)
+{
+	size_t half = size / 2, i = 0, j = 0, k;
+
+	if (size < 2)
+		return;
+
+	merge(array, temp, half);
+	merge(array + half, temp + half, size - half);
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array, half);
+	printf("[right]: ");
+	print_array(array + half, size - half);
+	for (k = 0; k < size; k++)
+		if (j >= size - half || (i < half && array[i] < (array + half)[j]))
+		{
+			temp[k] = array[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = (array + half)[j];
+			j++;
+		}
+	for (k = 0; k < size; k++)
+		array[k] = temp[k];
+	printf("[Done]: ");
+	print_array(array, size);
 }
---
+++ 104-heap_sort.c
@@ -0,0 +1,95 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+/**
+ * heap_sort - Sorting array using heap sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ * Return: 0
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = size / 2; i >= 0; i--)
+		heap(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
+	{
+		swap(&array[i], &array[0]);
+		if (i != 0)
+			print_array(array, size);
+		heap(array, i, 0, size);
+	}
+}
+
+/**
+ * heapify - Recursive function to sort binary tree
+ * @array: array to be sorted as binary tree
+ * @end: Last node in binary tree
+ * @start: First node of binary tree
+ * @size: Size of the array to sort
+ * Return: 0
+ */
+void heap(int *array, int end, int start, size_t size)
+{
+	int max = start;
+	int left = 2 * start + 1;
+	int right = 2 * start + 2;
+
+	if (!array || size < 2)
+		return;
+
+	if (left < end && array[left] > array[max])
+		max = left;
+
+	if (right < end && array[right] > array[max])
+		max = right;
+
+	if (start != max)
+	{
+		swap(&array[start], &array[max]);
+		print_array(array, size);
+		heap(array, end, max, size);
+	}
+}
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
