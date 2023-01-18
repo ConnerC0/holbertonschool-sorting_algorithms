5e2bdcbc2df7aeaa8a67a61b2312c3059ab6506e
ConnerC0 <connercurtis72@gmail.com>
2023-01-18 03:59:00 -0600

    setup files

---
+++ 103-merge_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * merge_sort - sorts the array with the merge algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+
+
+void merge_sort(int *array, size_t size)
+{
+
+}
